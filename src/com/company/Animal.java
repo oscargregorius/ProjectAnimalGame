f4190fd776787e6dd404ee2ea85716afe234adb9
oscargregorius <oscar.gregorius@gmail.com>
2020-10-02 11:28:51 +0200

    Makes the Animal class abstract and added some fields that are the same for all animals.

--- src/com/company/Animal.java
+++ src/com/company/Animal.java
@@ -1,4 +1,15 @@
 package com.company;
 
-public class Animal {
+public abstract class Animal {
+
+    String name;
+    String gender;
+    // Set all animals health to 100 from the beginning
+    int health = 100;
+    int price;
+
+    public Animal(String name, String gender){
+        this.name = name;
+        this.gender = gender;
+    }
 }
